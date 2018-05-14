5e6e3da17f913e8e8f40be46918212bd746a2e79
haein0303 <2849593@naver.com>
2018-05-14 10:00:53 +0900

    userIntefaceUpdate

---
+++ WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/USERINTERFACE.cpp
@@ -0,0 +1,24 @@
+#include "USERINTERFACE.h"
+
+//기본상태에서 다음 씬을 소환합니다.
+void USER_INTERFACE::nextScene() {
+	switch (USER_INTERFACE::now){
+	case TITLE:
+		USER_INTERFACE::now = MAIN_LOBBY;
+		break;
+	case MAIN_LOBBY:
+		USER_INTERFACE::now = SELECT_MODE;
+		break;
+	case SELECT_MODE:
+		USER_INTERFACE::now = SELECT_CHAR;
+	case SELECT_CHAR:
+		USER_INTERFACE::now = INGAME;
+		break;
+	case INGAME:
+		USER_INTERFACE::now = SCOREBOARD;
+		break;
+	case SCOREBOARD:
+		USER_INTERFACE::now = TITLE;
+		break;
+	}
+}
--- WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/USERINTERFACE.h
+++ WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/USERINTERFACE.h
@@ -1,4 +1,4 @@
-#pragma once
+#include <iostream>
 //==========================================================
 //               USERINTERFACE
 
@@ -13,13 +13,21 @@ enum SceneNAME {
 	TITLE = 0, MAIN_LOBBY = 1, CONTROL_TIP = 2, SELECT_MODE = 3, SELECT_CHAR = 4, INGAME = 5, SCOREBOARD = 6
 };
 
+bool gameState = false;
+
+
+
 class USER_INTERFACE {
 private:
 	//타이틀과 관련된 데이터 필드입니다.
+	SceneNAME now = TITLE;
+
 
 
 
 public:
+	//기본상태에서 다음 씬을 소환합니다.
+	void nextScene();
 
 	
 
