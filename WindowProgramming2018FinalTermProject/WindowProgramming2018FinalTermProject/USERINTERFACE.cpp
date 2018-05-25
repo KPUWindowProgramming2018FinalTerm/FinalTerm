b035b97f66ea99cd59324da8b8b4317f58f62ba7
haein0303 <2849593@naver.com>
2018-05-26 03:34:38 +0900

    Sorry

--- WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/USERINTERFACE.cpp
+++ WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/USERINTERFACE.cpp
@@ -1,5 +1,37 @@
 #include "USERINTERFACE.h"
 
+//생성자
+USER_INTERFACE::USER_INTERFACE() {
+	
+	//=====================================
+	//         위치 데이터
+	//=====================================
+	//메인로비
+	//TIP
+	R_MainLobby[TIP].top = 600;
+	R_MainLobby[TIP].left = 1600;
+	R_MainLobby[TIP].right = 250;
+	R_MainLobby[TIP].bottom = 106;
+
+	//START
+	R_MainLobby[START].top = 720;
+	R_MainLobby[START].left = 1600;
+	R_MainLobby[START].right = 250;
+	R_MainLobby[START].bottom = 106;
+
+	//EXIT
+	R_MainLobby[EXIT].top = 840;
+	R_MainLobby[EXIT].left = 1600;
+	R_MainLobby[EXIT].right = 250;
+	R_MainLobby[EXIT].bottom = 106;
+
+	//------------------------------
+
+
+
+	//===================================
+}
+
 SceneNAME USER_INTERFACE::returnScene() // 현재 씬 네임을 반환
 {
 	return Scene;
@@ -12,24 +44,24 @@ void USER_INTERFACE::ALLReset()
 
 void USER_INTERFACE::nextScene() //다음 씬을 불러온다
 {
-	switch (USER_INTERFACE::Scene){
+	switch (Scene){
 	case TITLE:
-		USER_INTERFACE::Scene = MAIN_LOBBY;
+		Scene = MAIN_LOBBY;
 		break;
 	case MAIN_LOBBY:
-		USER_INTERFACE::Scene = SELECT_MODE;
+		Scene = SELECT_MODE;
 		break;
 	case SELECT_MODE:
-		USER_INTERFACE::Scene = SELECT_CHAR;
+		Scene = SELECT_CHAR;
 		break;
 	case SELECT_CHAR:
-		USER_INTERFACE::Scene = INGAME;
+		Scene = INGAME;
 		break;
 	case INGAME:
-		USER_INTERFACE::Scene = SCOREBOARD;
+		Scene = SCOREBOARD;
 		break;
 	case SCOREBOARD:
-		USER_INTERFACE::Scene = TITLE;
+		Scene = TITLE;
 		break;
 	}
 }
@@ -42,4 +74,79 @@ bool USER_INTERFACE::checkInside(int x, int y, RECT rect) {
 	else {
 		return false;
 	}
+}
+
+//컨트롤 팁을 띄웁니다.
+void USER_INTERFACE::CallTip() {
+	if (Scene == MAIN_LOBBY) {
+		Scene = CONTROL_TIP;
+	}
+	else {
+		Scene = MAIN_LOBBY;
+	}	
+}
+
+//UI를 선택합니다.
+void USER_INTERFACE::SelectUI(WPARAM wParam) {
+	//SCENE에 따라서 명령을 다르게 실행합니다.
+	switch (Scene) {
+	case MAIN_LOBBY:
+		switch (wParam) {
+		case VK_UP:
+			switch (ML_Choice) {
+			case TIP:
+				ML_Choice = START;
+				break;
+			case START:
+				ML_Choice = EXIT;
+				break;
+			case EXIT:
+				ML_Choice = TIP;
+				break;
+			}		
+			break;
+		case VK_DOWN:
+			switch (ML_Choice) {
+			case TIP:
+				ML_Choice = EXIT;
+				break;
+			case START:
+				ML_Choice = TIP;
+				break;
+			case EXIT:
+				ML_Choice = START;
+				break;
+			}
+			break;
+		}
+		break;
+	case SELECT_MODE:
+		break;
+
+	}
+}
+
+int USER_INTERFACE::ReturnChoice() {
+	switch (Scene)
+	{
+	case MAIN_LOBBY:
+		return ML_Choice;
+		break;
+	case SELECT_MODE:
+		return 1;
+		break;
+	}
+}
+
+RECT USER_INTERFACE::GetRect(int choice) {
+	switch (Scene)
+	{
+	case MAIN_LOBBY:
+		return R_MainLobby[choice];
+		break;
+	case SELECT_MODE:
+		break;
+	}
+
+
 }
--- WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/main.cpp
+++ WindowProgramming2018FinalTermProject/WindowProgramming2018FinalTermProject/main.cpp
@@ -57,10 +57,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpszCmdPar
 
 CImage sample; // 이미지를 관리하는 클래스.
 
+CImage C_TITLE;
+CImage C_MainLobby_BG, C_MainLobby_START[2], C_MainLobby_TIP[2], C_MainLobby_EXIT[2];
+CImage C_TIP;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
 	static WCHAR text[100];
+	
+
 
 	switch (iMessage)
 	{
@@ -69,6 +75,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		
 		
 		sample.Load(L"Sample\\Laharl_alphaTest.png"); //이미지 연결. 전용 DC와 Bitmap이 연결되어있는 상태.
+		//타이틀 이미지를 로드합시다.
+		C_TITLE.Load(L"Graphic\\UI\\Title.jpg");
+		
+		//메인 로비 이미지를 로드 합시다.
+		C_MainLobby_BG.Load(L"Graphic\\UI\\MainLobby_BG.jpg");
+		C_MainLobby_START[0].Load(L"Graphic\\UI\\START.png");
+		C_MainLobby_START[1].Load(L"Graphic\\UI\\START2.png");
+
+		C_MainLobby_TIP[0].Load(L"Graphic\\UI\\TIP.png");
+		C_MainLobby_TIP[1].Load(L"Graphic\\UI\\TIP2.png");
+
+		C_MainLobby_EXIT[0].Load(L"Graphic\\UI\\EXIT.png");
+		C_MainLobby_EXIT[1].Load(L"Graphic\\UI\\EXIT2.png");
+
+		C_TIP.Load(L"Graphic\\UI\\HINT.jpg");
 		break;
 	case WM_PAINT: //Paint 메세지 불렸을 때
 		hDC = BeginPaint(hWnd, &ps);
@@ -77,19 +98,34 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 
 		//sample.Draw(hDC, clientRECT);																//Cimage를 잘 못그릴때는 Draw해주면 알아서 그려준다
 
+
 		switch (UI.returnScene())
 		{
 		case TITLE:
-			wsprintf(text, L"%d", UI.returnScene());
-			TextOut(hDC, clientRECT.right / 2, clientRECT.bottom / 2, text, 1);
+			C_TITLE.Draw(hDC, clientRECT);
 			break;
 		case MAIN_LOBBY:
-			wsprintf(text, L"%d", UI.returnScene());
-			TextOut(hDC, clientRECT.right / 2, clientRECT.bottom / 2, text, 1);
+			C_MainLobby_BG.Draw(hDC, clientRECT);
+			C_MainLobby_TIP[0].Draw(hDC,UI.GetRect(TIP));
+			C_MainLobby_START[0].Draw(hDC, UI.GetRect(START));
+			C_MainLobby_EXIT[0].Draw(hDC, UI.GetRect(EXIT));
+
+			switch (UI.ReturnChoice()) {
+			case TIP:
+				C_MainLobby_TIP[1].Draw(hDC, UI.GetRect(TIP));
+				break;
+			case START:
+				C_MainLobby_START[1].Draw(hDC, UI.GetRect(START));
+				break;
+			case EXIT:
+				C_MainLobby_EXIT[1].Draw(hDC, UI.GetRect(EXIT));
+				break;
+			}
+
+
 			break;
 		case CONTROL_TIP:
-			wsprintf(text, L"%d", UI.returnScene());
-			TextOut(hDC, clientRECT.right / 2, clientRECT.bottom / 2, text, 1);
+			C_TIP.Draw(hDC, clientRECT);
 			break;
 		case SELECT_MODE:
 			wsprintf(text, L"%d", UI.returnScene());
@@ -140,7 +176,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 			break;
 
 		case CONTROL_TIP:
-			UI.nextScene();
+			UI.CallTip();
 			break;
 
 		case SELECT_MODE:
